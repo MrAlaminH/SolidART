46f9d8ac50eb5417211dfa7115b4e58a8b33dbf3
Its-Alamin-H <alaminhosssain97@gmail.com>
2024-09-28 01:07:10 +0600

    fix: authjs and daynamic setting data changes

--- auth.ts
+++ auth.ts
@@ -1,35 +1,68 @@
-// authjs
 import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
 import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcryptjs";
 
 const prisma = new PrismaClient();
 
-export const { handlers, signIn, signOut, auth } = NextAuth({
-  theme: {
-    logo: "/logo.png",
-  },
+if (!process.env.AUTH_GOOGLE_ID || !process.env.AUTH_GOOGLE_SECRET) {
+  throw new Error("Google OAuth environment variables are missing.");
+}
+
+export const {
+  handlers: { GET, POST },
+  auth,
+  signIn,
+  signOut,
+} = NextAuth({
   adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: process.env.AUTH_GOOGLE_ID,
       clientSecret: process.env.AUTH_GOOGLE_SECRET,
     }),
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          return null;
+        }
+
+        const email = credentials.email as string;
+        const password = credentials.password as string;
+
+        const user = await prisma.user.findUnique({
+          where: { email },
+        });
+
+        if (!user || !user.password || typeof user.password !== 'string') {
+          return null;
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+
+        if (!isPasswordValid) {
+          return null;
+        }
+
+        return {
+          id: user.id,
+          email: user.email,
+          name: user.name,
+        };
+      },
+    }),
   ],
-  pages: {
-    signIn: "/",
-    signOut: "/auth/signout",
+  session: {
+    strategy: "jwt",
   },
-  callbacks: {
-    async redirect({ url, baseUrl }: { url: string, baseUrl: string }) {
-      // Allow relative URLs
-      if (url.startsWith("/")) return `${baseUrl}${url}`;
-      // Allow URLs that start with the base URL
-      if (url.startsWith(baseUrl)) return url;
-      // Otherwise, redirect to the base URL
-      return baseUrl;
-    },
+  pages: {
+    signIn: '/auth/signin',
   },
 });
-
